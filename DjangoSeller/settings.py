49fa97531d87a1ce2c2c64515b2a22ca73d2bb31
Demonica22 <demonica2003@mail.ru>
2022-11-01 18:15:29 +0300

    -some style shit

--- DjangoSeller/settings.py
+++ DjangoSeller/settings.py
@@ -117,6 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    (BASE_DIR / 'static')
+]
+
 MEDIA_URL = '/media/'
 LOGIN_URL = '/users/login/'
 LOGIN_REDIRECT_URL = "/products"
